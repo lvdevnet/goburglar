81922e21921f907fdbf3c22efe291b129acc08e2
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2015-06-04 00:04:54 +0300

    go fmt

--- src/burglar/burglar.go
+++ src/burglar/burglar.go
@@ -1,31 +1,31 @@
 package burglar
 
 import (
-	"strings"
-	"time"
-	"io"
-	"fmt"
-	"regexp"
+	"appengine"
+	"appengine/blobstore"
+	"appengine/channel"
+	"appengine/datastore"
+	"appengine/image"
+	"appengine/taskqueue"
+	"appengine/urlfetch"
+	"appengine/user"
 	"crypto/md5"
 	"crypto/rand"
 	"encoding/hex"
-	"net/http"
-	"net/url"
-	"html/template"
-	"appengine"
-	"appengine/user"
-	"appengine/urlfetch"
-	"appengine/channel"
-	"appengine/datastore"
-	"appengine/taskqueue"
-	"appengine/blobstore"
-	"appengine/image"
-	newappengine "google.golang.org/appengine"
+	"fmt"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
+	newappengine "google.golang.org/appengine"
 	"google.golang.org/cloud"
 	"google.golang.org/cloud/storage"
+	"html/template"
+	"io"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strings"
+	"time"
 )
 
 func init() {
@@ -65,6 +65,7 @@ type Thumbnail struct {
 }
 
 var templates *template.Template = nil
+
 const cookieName = "image-scrap-clientid"
 const rootNode = "image-scrap-request"
 const thumbnailLeaf = "image-scrap-thumbnail"
@@ -81,12 +82,12 @@ func blobkey(filename string, c appengine.Context) (appengine.BlobKey, error) {
 func storagectx(c appengine.Context, r *http.Request) context.Context {
 	nc := newappengine.NewContext(r)
 	h := &http.Client{
-        Transport: &oauth2.Transport{
-            Source: google.AppEngineTokenSource(nc, storage.ScopeFullControl),
-            Base:   &urlfetch.Transport{Context: c},
-        },
-    }
-    return cloud.WithContext(nc, appengine.AppID(c), h)
+		Transport: &oauth2.Transport{
+			Source: google.AppEngineTokenSource(nc, storage.ScopeFullControl),
+			Base:   &urlfetch.Transport{Context: c},
+		},
+	}
+	return cloud.WithContext(nc, appengine.AppID(c), h)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -95,7 +96,9 @@ func index(w http.ResponseWriter, r *http.Request) {
 		c := appengine.NewContext(r)
 		clientId := cookie.Value
 		token, err := channel.Create(c, clientId)
-		if error3(err, c, w) { return }
+		if error3(err, c, w) {
+			return
+		}
 		gallery(token, c, w)
 	} else {
 		http.Redirect(w, r, "/static/start.html", http.StatusTemporaryRedirect)
@@ -132,7 +135,9 @@ func gallery(token string, c appengine.Context, w http.ResponseWriter) {
 	if templates == nil {
 		var err error
 		templates, err = template.ParseFiles("templates/gallery.html")
-		if error3(err, c, w) { return }
+		if error3(err, c, w) {
+			return
+		}
 	}
 	w.Header().Set("Content-Type", "text/html")
 	templates.ExecuteTemplate(w, "gallery.html", token)
@@ -165,12 +170,14 @@ func iterate(clientId *string, c appengine.Context, cc context.Context, op strin
 		if err == datastore.Done {
 			break
 		}
-		if error2(err, c) { return }
+		if error2(err, c) {
+			return
+		}
 		rkeys = append(rkeys, key)
 	}
 	if clientId == nil { // cleanup
 		allKeys = append(allKeys, rkeys...)
-		rkeys = []*datastore.Key{ nil }
+		rkeys = []*datastore.Key{nil}
 	}
 	for _, rkey := range rkeys {
 		err, tkeys, blobs := iterate2(clientId, rkey, c, op)
@@ -183,7 +190,9 @@ func iterate(clientId *string, c appengine.Context, cc context.Context, op strin
 			allKeys = append(allKeys, *tkeys...)
 			allBlobs = append(allBlobs, *blobs...)
 		}
-		if error2(err, c) { break }
+		if error2(err, c) {
+			break
+		}
 	}
 	if op == "delete" {
 		bkt := bucket(c)
@@ -213,7 +222,9 @@ func iterate2(clientId *string, rkey *datastore.Key, c appengine.Context, op str
 		if err == datastore.Done {
 			break
 		}
-		if error2(err, c) { return err, &keys, &blobs }
+		if error2(err, c) {
+			return err, &keys, &blobs
+		}
 		if op == "send" {
 			c.Debugf("pushing from db '%v'", thumbnail.ThumbnailURL)
 			channel.Send(c, *clientId, thumbnail.ThumbnailURL)
@@ -237,12 +248,16 @@ func start(w http.ResponseWriter, r *http.Request) {
 
 	client := urlfetch.Client(c)
 	resp, err := client.Get("http://" + target)
-	if error3(err, c, w) { return }
+	if error3(err, c, w) {
+		return
+	}
 
 	len := int(resp.ContentLength)
 	buf := make([]byte, len)
 	read, err := resp.Body.Read(buf)
-	if error3(err, c, w) { return }
+	if error3(err, c, w) {
+		return
+	}
 	if read != len {
 		http.Error(w, fmt.Sprintf("Target page Content-Length is %v but read %v bytes", len, read), http.StatusInternalServerError)
 		return
@@ -265,16 +280,22 @@ func start(w http.ResponseWriter, r *http.Request) {
 		len := 16
 		r := make([]byte, len)
 		read, err := rand.Read(r)
-		if error3(err, c, w) && read < 8 { return }
+		if error3(err, c, w) && read < 8 {
+			return
+		}
 		clientId = hex.EncodeToString(r)
 	}
 
 	token, err := channel.Create(c, clientId)
-	if error3(err, c, w) { return }
+	if error3(err, c, w) {
+		return
+	}
 
 	isr := Request{clientId}
 	key, err := datastore.Put(c, datastore.NewIncompleteKey(c, rootNode, nil), &isr)
-	if error3(err, c, w) { return }
+	if error3(err, c, w) {
+		return
+	}
 
 	seen := make(map[string]struct{})
 	for _, image := range images {
@@ -284,7 +305,9 @@ func start(w http.ResponseWriter, r *http.Request) {
 				seen[addr] = struct{}{}
 				task := taskqueue.NewPOSTTask("/fetch", url.Values{"clientId": {clientId}, "image": {addr}, "key": {key.Encode()}})
 				_, err := taskqueue.Add(c, task, "default")
-				if error3(err, c, w) { return }
+				if error3(err, c, w) {
+					return
+				}
 			}
 		}
 	}
@@ -302,11 +325,15 @@ func fetch(w http.ResponseWriter, r *http.Request) {
 	imageUrl := r.FormValue("image")
 	c.Debugf("fetching '%v'", imageUrl)
 	key, err := datastore.DecodeKey(r.FormValue("key"))
-	if error3(err, c, w) { return }
+	if error3(err, c, w) {
+		return
+	}
 
 	client := urlfetch.Client(c)
 	resp, err := client.Get(imageUrl)
-	if error3(err, c, w) { return }
+	if error3(err, c, w) {
+		return
+	}
 	c.Debugf("downloaded '%v'", imageUrl)
 
 	bkt := bucket(c)
@@ -316,20 +343,28 @@ func fetch(w http.ResponseWriter, r *http.Request) {
 	blob.ContentType = resp.Header.Get("Content-Type")
 	blob.ACL = []storage.ACLRule{{storage.AllUsers, storage.RoleReader}}
 	written, err := io.Copy(blob, resp.Body)
-	if error3(err, c, w) { return }
+	if error3(err, c, w) {
+		return
+	}
 	err = blob.Close()
-	if error3(err, c, w) { return }
-	if (written < 100) {
+	if error3(err, c, w) {
+		return
+	}
+	if written < 100 {
 		c.Infof("image is too small: %v bytes; deleting", written)
 		storage.DeleteObject(cc, bkt, filename)
 		return
 	}
 
 	blobkey, err := blobkey(filename, c)
-	if error3(err, c, w) { return }
+	if error3(err, c, w) {
+		return
+	}
 
 	thumbnailUrl, err := image.ServingURL(c, blobkey, &image.ServingURLOptions{Size: 100})
-	if error3(err, c, w) { return }
+	if error3(err, c, w) {
+		return
+	}
 	thumbnail := thumbnailUrl.String()
 
 	c.Debugf("pushing to client '%v'", thumbnail)
@@ -337,5 +372,7 @@ func fetch(w http.ResponseWriter, r *http.Request) {
 
 	ist := Thumbnail{thumbnail, filename}
 	_, err = datastore.Put(c, datastore.NewIncompleteKey(c, thumbnailLeaf, key), &ist)
-	if error3(err, c, w) { return }
+	if error3(err, c, w) {
+		return
+	}
 }
